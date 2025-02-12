49c7b7d53c61bf01234d3e096a72d530d6ed088e
arman-devops7 <armanmukadamdevops@gmail.com>
2025-02-12 14:12:00 +0530

    added conditional statements

---
+++ conditional_statements.py
@@ -0,0 +1,56 @@
+# if elif and else statements
+# age=21
+
+# if(age>=18):
+#     print("age is greater than or equal to 18")
+#     print("can vote")
+#     if(age==21):
+#         print("age is 21")
+# else:
+#     print("cannot vote")
+
+
+# light="green"
+
+# if(light=="red"):
+#     print("stop")
+# elif(light=="yellow"):
+#     print("slow")
+# elif(light=="green"):
+#     print("go")
+# else:
+#     print("not found")
+
+# print("end of code")
+
+# program
+# find odd and even
+# num=int(input("Enter the no"))
+
+# # chck remainder is 0
+# if(num%2==0):
+#     print("Even Number",num)
+# else:
+#     print("Odd Number",num)
+
+# find the greatest of 3 numbers entered by the user
+
+# a=int(input("a -> "))
+# b=int(input("b -> "))
+# c=int(input("c -> "))
+
+# if (a>= b and a >=b):
+#     print("first no is greatest",a)
+# elif(b>=c):
+#     print("second is greatest",b)
+# else:
+#     print("thirs is large",c)
+
+# check if a number is multiple of 7 or not
+
+num=int(input("enter num"))
+
+if (num%7==0):
+    print("the no is a multiple of 7")
+else:
+    print("Not multiple of 7")
